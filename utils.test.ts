761412fc6a877d73a3ec3f80c702bcb03e8a3d5c
Maxwell Davish <mdavish@yext.com>
2023-06-30 09:53:44 -0400

    tests for phone numbers

---
+++ utils.test.ts
@@ -0,0 +1,18 @@
+import { formatPhoneNumber } from "./utils";
+
+const somePhoneNumbers = [
+  "+44 7803 803962",
+  "+49 151 10678260",
+  "203-921-6015",
+  "2155346876",
+];
+
+function main() {
+  somePhoneNumbers.forEach((phoneNumber) => {
+    console.log(
+      `formatting ${phoneNumber} to ${formatPhoneNumber(phoneNumber)}`
+    );
+  });
+}
+
+main();
